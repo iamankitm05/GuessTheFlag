9ffe5e24a15ee37996e8fff7e313d9e135268f3f
Ankit kumar <iamankitm05@gmail.com>
2025-11-10 21:25:12 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Ankit Kumar Ojha on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
