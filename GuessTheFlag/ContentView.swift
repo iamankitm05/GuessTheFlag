f783a6d612c87db90bb010dc286f1c1b731f8f43
Ankit kumar <iamankitm05@gmail.com>
2025-11-12 23:03:52 +0530

    Changes Done:  - Showing score - Showing worng answer message - Restarting game after 8 question

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -8,14 +8,78 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var scoreMessage = ""
+    @State private var score = 0
+    @State private var currentQuestion = 1
+    
+    private let maxQuestions = 10
+    
+    var isGameOver: Bool {
+        currentQuestion >= maxQuestions
+    }
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            LinearGradient(colors: [.blue, .black], startPoint: .top, endPoint: .bottom).ignoresSafeArea(edges: .all)
+            
+                VStack(spacing: 30) {
+                VStack {
+                    Text("Tap the flag of")
+                        .foregroundStyle(.white)
+                        .font(.subheadline.weight(.heavy))
+                    
+                    Text(countries[correctAnswer])
+                        .foregroundStyle(.white)
+                        .font(.largeTitle.weight(.semibold))
+                }
+                
+                ForEach(0 ..< 3) { number in
+                    Button {
+                        flagTapped(number)
+                    } label: {
+                        Image(countries[number])
+                            .clipShape(.capsule)
+                            .shadow(radius: 5)
+                    }
+                }
+                
+            }
+        }.alert(scoreTitle, isPresented: $showingScore) {
+            Button("Continue", action: askQuestion)
+        } message: {
+            Text(scoreMessage)
         }
-        .padding()
+    }
+    
+    func flagTapped(_ number: Int) {
+        if number == correctAnswer {
+            score += 1;
+            scoreTitle = isGameOver ? "Finished" : "Correct"
+            scoreMessage = "Your score is \(score). "
+        } else {
+            scoreTitle = isGameOver ? "Finished" : "Wrong"
+            scoreMessage = "Wrong! That’s the flag of \(countries[number]). "
+        }
+
+        scoreMessage += isGameOver ? "Play again?" : "Keep going!"
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        if(isGameOver) {
+            score = 0
+            currentQuestion = 1
+        } else {
+            currentQuestion += 1;
+        }
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
     }
 }
 
